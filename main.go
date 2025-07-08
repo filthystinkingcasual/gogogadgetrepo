812e7574049120f6ea99f934712fb72a77fd0377
Felicia Lyn-Shue <flynshue@gmail.com>
2025-07-08 14:52:29 -0400

    starting using bulma and add more config properties

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"text/template"
 
 	"github.com/spf13/viper"
@@ -21,18 +23,43 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write(content)
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("views/home.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	repoUrl := viper.GetString("gitrepo")
+	u, _ := url.Parse(repoUrl)
+	repoName := strings.TrimLeft(u.Path, "/")
+	data := struct {
+		AppName string
+		GitRepo string
+		GitURL  string
+	}{
+		AppName: viper.GetString("appName"),
+		GitURL:  viper.GetString("gitrepo"),
+		GitRepo: repoName,
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
 func config(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("views/config.html")
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	hostname := viper.GetString("hostname")
-	t.Execute(w, hostname)
+	settings := viper.AllSettings()
+	t.Execute(w, settings)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", home)
 	http.HandleFunc("/config", config)
 	port := viper.GetString("port")
 	log.Printf("Starting sever on :%s", port)
