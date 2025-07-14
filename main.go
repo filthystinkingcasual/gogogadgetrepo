38702c4d0ae1dff0257bb04c57bdbfbd46f380f5
Mara W <37845942+filthystinkingcasual@users.noreply.github.com>
2025-07-14 12:01:37 -0400

    adding prettiness

--- main.go
+++ main.go
@@ -4,35 +4,59 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"net/url"
+
+	"strings"
 	"text/template"
 
 	"github.com/spf13/viper"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	content, err := os.ReadFile("index.html")
-	if err != nil {
-		http.Error(w, "It's broken. Just like my spirit.", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/html")
-	w.Write(content)
-}
-
 func config(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("views/config.html")
+	if err != nil {
+		http.Error(w, "lol no", http.StatusInternalServerError)
+		return
+	}
+	settings := viper.AllSettings()
+	t.Execute(w, settings)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("views/home.html")
+	if err != nil {
+		http.Error(w, "that didn't work out so well for you did it", http.StatusInternalServerError)
+		return
+	}
+	repoUrl := viper.GetString("gitrepo")
+	u, _ := url.Parse(repoUrl)
+	repoName := strings.TrimLeft(u.Path, "/")
+	data := struct {
+		AppName string
+		GitRepo string
+		GitURL  string
+		Item1   string
+		Item2   string
+		Item3   string
+		Item4   string
+	}{
+		AppName: viper.GetString("appName"),
+		GitURL:  viper.GetString("gitrepo"),
+		GitRepo: repoName,
+		Item1:   viper.GetString("configthefirst"),
+		Item2:   viper.GetString("configthesecond"),
+		Item3:   viper.GetString("configthethird"),
+		Item4:   viper.GetString("configthefourth"),
+	}
+	err = t.Execute(w, data)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	hostname := viper.GetString("hostname")
-	t.Execute(w, hostname)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", home)
 	http.HandleFunc("/config", config)
 	port := viper.GetString("port")
 	log.Printf("Starting sever on :%s", port)
